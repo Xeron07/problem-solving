b052e386ad7bfde2331c1a10b4b0504b4cff0bcb
Md. Minhaz Uddin <ni.xeron07@gmail.com>
2022-04-21 15:26:06 +0600

    worked and more faster than previous

--- leet code/palidrome string.py
+++ leet code/palidrome string.py
@@ -1,13 +1,8 @@
 def plaidromWord(list):
     for word in list:
-        flag=True
-        for i in range(0,len(word)):
-            if word[i] != word[-(i+1)]:
-                flag = False
-                break
-        if flag: return word
-
+        if word == word[::-1]:
+            return word
     return ""
 
 
-print(plaidromWord(["abc","car","ada","racecar","cool"]))
+print(plaidromWord(["def","ghi"]))
