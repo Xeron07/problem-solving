abb437f98234b3044b2424328618f29d323d5118
Md. Minhaz Uddin <ni.xeron07@gmail.com>
2022-05-19 23:28:41 +0600

    update

--- leet code/substring.py
+++ leet code/substring.py
@@ -17,7 +17,16 @@ def maxSubStr(str):
 
 
 def secondApproach(str):
+    start=0
+    map={}
+    maxLength=0
+    for end in range(0,len(str)):
+        if str[end] in map.keys():
+            start = max(map[str[end]]+1,start)
+            map[str[end]]=end
+        else:
+            map[str[end]]=end
+        maxLength = max((end - start) + 1, maxLength)
+    return maxLength
 
-
-
-print(maxSubStr(strI))
+print(secondApproach(strI))
